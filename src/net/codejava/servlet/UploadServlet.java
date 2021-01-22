0a2bc66002555361b3461512d77b6f2cd7706d9e
bppur <bppur@PURUSHOTH>
2021-01-22 12:25:48 +0530

    Pdf reader

---
+++ src/net/codejava/servlet/UploadServlet.java
@@ -0,0 +1,63 @@
+package net.codejava.servlet;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+
+@WebServlet("/UploadServlet")
+@MultipartConfig(fileSizeThreshold=1024*1024*2,	// 2MB
+				 maxFileSize=1024*1024*10,		// 10MB
+				 maxRequestSize=1024*1024*50)	// 50MB
+public class UploadServlet extends HttpServlet {
+
+
+	/**
+	 * handles file upload
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+
+		for (Part part : request.getParts()) {
+			String fileName = extractFileName(part);
+			//Loading the document uuploaded by user
+		      File file = new File(fileName);
+		      PDDocument document = PDDocument.load(file);
+		      //Instantiate PDFTextStripper class
+		      PDFTextStripper pdfStripper = new PDFTextStripper();
+		      //Retrieving text from PDF document
+		      String text = pdfStripper.getText(document);
+		      fileName = new File(fileName).getName();
+		      PrintStream o = new PrintStream(new File("D:\\"+fileName+".txt")); 
+		      System.setOut(o); 
+		      System.out.println(text);
+		      //Closing the document
+		      document.close();
+		}
+
+		request.setAttribute("message", "PDF-Text Conversion has been done successfully! File will be saved in you D-Drive");
+		getServletContext().getRequestDispatcher("/message.jsp").forward(
+				request, response);
+	}
+
+	
+	//Extracts file name from HTTP header content-disposition
+	private String extractFileName(Part part) {
+		String contentDisp = part.getHeader("content-disposition");
+		String[] items = contentDisp.split(";");
+		for (String s : items) {
+			if (s.trim().startsWith("filename")) {
+				return s.substring(s.indexOf("=") + 2, s.length()-1);
+			}
+		}
+		return "";
+	}
+}
